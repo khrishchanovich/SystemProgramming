74db7f7898186edfbc8f58cd2092bed2029fc5bc
khrishchanovich <orannon@gmail.com>
2023-10-24 21:15:35 +0300

    Complete Lab5 (Project5)

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -2,116 +2,105 @@
 #include <iostream>
 #include <string>
 
-bool RegisterProgram(const std::string& programName, const std::string& installerPath) {
-    HKEY hUninstallKey;
-    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, KEY_WRITE, &hUninstallKey) == ERROR_SUCCESS) {
+// Регистрируем новую программу в реестре с установкой пути к инсталятору
+bool SetProgramRegistry(const std::string& programName, const std::string& installerPath) {
+    HKEY hParentKey;
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, "SOFTWARE\\MyProject", 0, KEY_WRITE, &hParentKey) == ERROR_SUCCESS) {
         HKEY hAppKey;
-        if (RegCreateKey(hUninstallKey, programName.c_str(), &hAppKey) == ERROR_SUCCESS) {
+        if (RegCreateKey(hParentKey, programName.c_str(), &hAppKey) == ERROR_SUCCESS) {
             RegSetValueEx(hAppKey, "DisplayName", 0, REG_SZ, (const BYTE*)programName.c_str(), programName.size());
 
-            // Set UninstallString to an empty string
-            std::string emptyString = "";
-            RegSetValueEx(hAppKey, "UninstallString", 0, REG_SZ, (const BYTE*)emptyString.c_str(), emptyString.size());
+            RegSetValueEx(hAppKey, "InstallString", 1, REG_SZ, (const BYTE*)installerPath.c_str(), installerPath.size());
 
             RegCloseKey(hAppKey);
-            RegCloseKey(hUninstallKey);
-
-            // Now, run the program installer
-            SHELLEXECUTEINFO ShExecInfo = {0};
-            ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
-            ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
-            ShExecInfo.lpFile = installerPath.c_str();
-            ShExecInfo.nShow = SW_SHOWNORMAL;
-
-            if (ShellExecuteEx(&ShExecInfo) == TRUE) {
-                WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
-                CloseHandle(ShExecInfo.hProcess);
-                return true;
-            }
+            RegCloseKey(hParentKey);
+            return true;
         }
     }
     return false;
 }
 
-std::string FindUninstallPath(const std::string& programName) {
-    HKEY hUninstallKey;
-    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, KEY_READ, &hUninstallKey) == ERROR_SUCCESS) {
-        std::string uninstallPath;
-        DWORD uninstallPathSize = 1024;
-        if (RegGetValue(hUninstallKey, programName.c_str(), "UninstallString", RRF_RT_REG_SZ, NULL, &uninstallPath[0], &uninstallPathSize) == ERROR_SUCCESS) {
-            RegCloseKey(hUninstallKey);
-            return uninstallPath;
-        }
-        RegCloseKey(hUninstallKey);
+// Запускаем инсталятор
+bool RunInstaller(const std::string& installerPath) {
+    SHELLEXECUTEINFO ShExecInfo = {0};
+    ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+    ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+    ShExecInfo.lpFile = installerPath.c_str();
+    ShExecInfo.nShow = SW_SHOWNORMAL;
+
+    if (ShellExecuteEx(&ShExecInfo) == TRUE) {
+        WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
+        CloseHandle(ShExecInfo.hProcess);
+        return true;
     }
-    return "";
+    return false;
 }
 
-bool UninstallProgram(const std::string& programName) {
-    std::string uninstallPath;
+// Устаналиваем путь к деинсталятору
+bool SetUninstallRegistry(const std::string& programName, const std::string& uninstallerPath) {
+    HKEY hParentKey;
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, "SOFTWARE\\MyProject", 0, KEY_WRITE, &hParentKey) == ERROR_SUCCESS) {
+        HKEY hAppKey;
+        if (RegCreateKey(hParentKey, programName.c_str(), &hAppKey) == ERROR_SUCCESS) {
 
-    std::cout << "Enter the uninstall path (e.g., C:\\Path\\To\\Uninstaller.exe): ";
-    std::getline(std::cin, uninstallPath);
+            RegSetValueEx(hAppKey, "UninstallString", 2, REG_SZ, (const BYTE*)uninstallerPath.c_str(), uninstallerPath.size());
 
-    if (!uninstallPath.empty()) {
-        // Run the uninstaller
-        SHELLEXECUTEINFO ShExecInfo = {0};
-        ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
-        ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
-        ShExecInfo.lpFile = uninstallPath.c_str();
-        ShExecInfo.nShow = SW_SHOWNORMAL;
+            RegCloseKey(hAppKey);
+            RegCloseKey(hParentKey);
 
-        if (ShellExecuteEx(&ShExecInfo) == TRUE) {
-            WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
-            CloseHandle(ShExecInfo.hProcess);
-        } else {
-            std::cout << "Failed to run the uninstaller." << std::endl;
-            return false;
+            return true;
         }
+    }
+    return false;
+}
 
-        // Ask the user if they want to delete the program from the registry
-        std::cout << "Do you want to delete the program from the registry (yes/no)? ";
-        std::string answer;
-        std::cin >> answer;
+// Запускаем деинсталятор
+bool RunUninstaller(const std::string& programName) {
+    HKEY hParentKey;
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, "SOFTWARE\\MyProject", 0, KEY_READ, &hParentKey) == ERROR_SUCCESS) {
+        HKEY hAppKey;
+        if (RegOpenKeyEx(hParentKey, programName.c_str(), 0, KEY_READ, &hAppKey) == ERROR_SUCCESS) {
+            DWORD dataValueSize = 1024;
+            char uninstallerPath[1024];
+            if (RegQueryValueEx(hAppKey, "UninstallString", 0, NULL, (LPBYTE)uninstallerPath, &dataValueSize) == ERROR_SUCCESS) {
+                SHELLEXECUTEINFO ShExecInfo = {0};
+                ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+                ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+                ShExecInfo.lpFile = uninstallerPath;
+                ShExecInfo.nShow = SW_SHOWNORMAL;
 
-        if (answer == "yes" || answer == "Yes" || answer == "YES") {
-            // Update UninstallString in the registry
-            HKEY hUninstallKey;
-            if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, KEY_WRITE, &hUninstallKey) == ERROR_SUCCESS) {
-                if (RegDeleteKey(hUninstallKey, programName.c_str()) == ERROR_SUCCESS) {
-                    RegCloseKey(hUninstallKey);
-                    std::cout << "Program uninstalled and removed from the registry." << std::endl;
+                if (ShellExecuteEx(&ShExecInfo) == TRUE) {
+                    WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
+                    CloseHandle(ShExecInfo.hProcess);
                     return true;
+                } else {
+                    std::cout << "Failed to run the uninstaller.\n***\n" << std::endl;
+                    return false;
                 }
-                RegCloseKey(hUninstallKey);
             }
-        } else {
-            std::cout << "Program uninstalled but not removed from the registry." << std::endl;
+            RegCloseKey(hAppKey);
         }
+        RegCloseKey(hParentKey);
     }
     return false;
 }
 
+// Список подключей в ключе реестра
 void ListInstalledPrograms() {
-    HKEY hUninstallKey;
-    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, KEY_READ, &hUninstallKey) == ERROR_SUCCESS) {
+    HKEY hParentKey;
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, "SOFTWARE\\MyProject", 0, KEY_READ, &hParentKey) == ERROR_SUCCESS) {
         DWORD subkeyIndex = 0;
         char subkeyName[255];
 
-        while (RegEnumKey(hUninstallKey, subkeyIndex, subkeyName, sizeof(subkeyName)) == ERROR_SUCCESS) {
+        while (RegEnumKey(hParentKey, subkeyIndex, subkeyName, sizeof(subkeyName)) == ERROR_SUCCESS) {
             HKEY hAppKey;
-            if (RegOpenKeyEx(hUninstallKey, subkeyName, 0, KEY_READ, &hAppKey) == ERROR_SUCCESS) {
-                DWORD valueSize = 1024;
-                char value[1024];
-                if (RegQueryValueEx(hAppKey, "DisplayName", NULL, NULL, (LPBYTE)value, &valueSize) == ERROR_SUCCESS) {
-                    std::cout << "Installed Program: " << value << std::endl;
-                    std::cout << "Registry Key Path: SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" << subkeyName << std::endl;
-                }
+            if (RegOpenKeyEx(hParentKey, subkeyName, 0, KEY_READ, &hAppKey) == ERROR_SUCCESS) {
+                std::cout << "Program: " << subkeyName << std::endl;
                 RegCloseKey(hAppKey);
             }
             subkeyIndex++;
         }
-        RegCloseKey(hUninstallKey);
+        RegCloseKey(hParentKey);
     }
 }
 
@@ -120,11 +109,14 @@ int main() {
         int choice;
         std::string programName;
         std::string installerPath;
+        std::string uninstallerPath;
 
         std::cout << "1. List installed programs\n";
-        std::cout << "2. Register a new program\n";
-        std::cout << "3. Unregister a program\n";
-        std::cout << "4. Exit\n";
+        std::cout << "2. Register new program and set InstallerPath\n";
+        std::cout << "3. Set UninstallerPath\n";
+        std::cout << "4. Install program\n";
+        std::cout << "5. Uninstall program\n";
+        std::cout << "6. Exit\n";
         std::cout << "Enter your choice: ";
         std::cin >> choice;
 
@@ -132,28 +124,71 @@ int main() {
             ListInstalledPrograms();
         } else if (choice == 2) {
             std::cout << "Enter the program name: ";
-            std::cin.ignore();
             std::getline(std::cin, programName);
 
             std::cout << "Enter the installer path (e.g., C:\\Path\\To\\Installer.exe): ";
             std::getline(std::cin, installerPath);
 
-            if (RegisterProgram(programName, installerPath)) {
-                std::cout << "Program registered successfully!" << std::endl;
+            if (SetProgramRegistry(programName, installerPath)) {
+                std::cout << "Program information added to the registry." << std::endl;
             } else {
-                std::cout << "Failed to register the program." << std::endl;
+                std::cout << "Failed to add program information to the registry." << std::endl;
             }
         } else if (choice == 3) {
-            std::cout << "Enter the program name to uninstall: ";
-            std::cin.ignore();
+            std::cout << "Enter the program name to set UninstallString ";
             std::getline(std::cin, programName);
 
-            if (UninstallProgram(programName)) {
-                std::cout << "Program uninstalled successfully!" << std::endl;
+            std::cout << "Enter the uninstaller path (e.g., C:\\Path\\To\\Uninstaller.exe): ";
+            std::getline(std::cin, uninstallerPath);
+
+            if (SetUninstallRegistry(programName, uninstallerPath)) {
+                std::cout << "Uninstall information added to the registry." << std::endl;
             } else {
-                std::cout << "Failed to uninstall the program." << std::endl;
+                std::cout << "Failed to add uninstall information to the registry." << std::endl;
             }
         } else if (choice == 4) {
+            std::cout << "Enter the program name to install: ";
+            std::getline(std::cin, programName);
+
+            HKEY hInstallKey;
+            if (RegOpenKeyEx(HKEY_CURRENT_USER, "SOFTWARE\\MyProject", 0, KEY_READ, &hInstallKey) == ERROR_SUCCESS) {
+                HKEY hAppKey;
+                if (RegOpenKeyEx(hInstallKey, programName.c_str(), 0, KEY_READ, &hAppKey) == ERROR_SUCCESS) {
+                    DWORD valueSize = 1024;
+                    char installPath[1024];
+                    if (RegQueryValueEx(hAppKey, "InstallString", 0, NULL, (LPBYTE)installPath, &valueSize) == ERROR_SUCCESS) {
+                        if (RunInstaller(installPath)) {
+                            std::cout << "Program installed successfully!" << std::endl;
+                        } else {
+                            std::cout << "Failed to run the installer." << std::endl;
+                        }
+                    }
+                    RegCloseKey(hAppKey);
+                }
+                RegCloseKey(hInstallKey);
+            }
+        } else if (choice == 5) {
+            std::cout << "Enter the program name to uninstall: ";
+            std::getline(std::cin, programName);
+
+            HKEY hUninstallKey;
+            if (RegOpenKeyEx(HKEY_CURRENT_USER, "SOFTWARE\\MyProject", 0, KEY_READ, &hUninstallKey) == ERROR_SUCCESS) {
+                HKEY hAppKey;
+                if (RegOpenKeyEx(hUninstallKey, programName.c_str(), 0, KEY_READ, &hAppKey) == ERROR_SUCCESS) {
+                    DWORD valueSize = 1024;
+                    char uninstallPath[1024];
+                    if (RegQueryValueEx(hAppKey, "UninstallString", 0, NULL, (LPBYTE)uninstallPath, &valueSize) == ERROR_SUCCESS) {
+                        if (RunUninstaller(programName)) {
+                            std::cout << "Program uninstalled successfully!" << std::endl;
+                        } else {
+                            std::cout << "Failed to run the uninstaller." << std::endl;
+                        }
+                    }
+                    RegCloseKey(hAppKey);
+                }
+                RegCloseKey(hUninstallKey);
+            }
+        } else if (choice == 6) {
             break;
         }
     }
